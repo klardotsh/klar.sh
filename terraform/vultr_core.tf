65b67705772bdbc555bf0d1e8fed2b4985e2aae2
Josh Klar <josh@klar.sh>
2023-07-29 15:37:58 -0700

    terraform: remove unused Vultr resources

--- terraform/vultr_core.tf
+++ terraform/vultr_core.tf
@@ -5,19 +5,3 @@ resource "vultr_ssh_key" "my_ssh_key" {
   # trims these server-side, and not trimming here results in a diff in the
   # plan on *every* deploy. noisy!
 }
-
-resource "vultr_iso_private" "void_x8664_musl" {
-  url = "https://alpha.de.repo.voidlinux.org/live/current/void-live-x86_64-musl-20191109.iso"
-}
-
-resource "vultr_reserved_ip" "eip_v4" {
-  label     = "eip_v4-1"
-  region_id = data.vultr_region.amsterdam.id
-  ip_type   = "v4"
-}
-
-resource "vultr_network" "klardotsh_vpc" {
-  description = "klardotsh's VPC"
-  region_id   = data.vultr_region.amsterdam.id
-  cidr_block  = "10.0.0.0/24"
-}
